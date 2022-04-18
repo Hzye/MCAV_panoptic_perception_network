518fe09c2490c3c6e77eb2fada3f3f7aff00ddd8
Hzye <81663786+Hzye@users.noreply.github.com>
2022-04-18 20:15:30 +1000

    detection loop

--- detector.py
+++ detector.py
@@ -147,4 +147,60 @@ if (len(im_dims_list) % batch_size):
 if batch_size != 1:
     n_batches = len(img_list)//batch_size + leftover
     img_batches = [torch.cat((img_batches[i*batch_size:min((i+1)*batch_size, len(img_batches))])) for i in range(n_batches)]
+
+
+## detection loop
+# 1. iterate over batches
+# 2. make prediction
+# 3. cat prediction tensors (Dx8 from write_results()) of all images we have to perform detections on
+
+# measure time taken for det as time between taking input and producing output of write_results()
+#   - one attr in write_prediction() is the index of img in batch
+#   - transform this attr to rep index of image in img_list, which contains addresses of all imgs
+#   - print time taken for each det + obj detected
+write = 0
+start_det_loop = time.time()
+for idx, batch in enumerate(img_batches):
+    # load image
+    start = time.time()
+    if CUDA:
+        batch = batch.cuda()
     
+    prediction = model(Variable(batch, volatile=True), CUDA)
+    prediction = write_results(
+        prediction=prediction,
+        confidence=confidence,
+        n_classes=n_classes,
+        nms_conf=nms_thresh
+    )
+    end = time.time()
+
+    if type(prediction) == int:
+        for img_num, image in enumerate(img_list[idx*batch_size:min((idx+1)*batch_size, len(img_list))]):
+            img_id = idx*batch_size + img_num
+            print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/batch_size))
+            print("{0:20s} {1:s}".format("Objects Detected:", ""))
+            print("----------------------------------------------------------")
+        continue
+    
+    # transform attr from index in batch to index in img_list
+    prediction[:,0] += idx*batch_size
+
+    # if output hasnt been init
+    if not write:
+        output = prediction
+        write = 1
+    else:
+        output = torch.cat((output, prediction))
+    
+    for img_num, image in enumerate(img_list[idx*batch_size:min((idx+1)*batch_size, len(img_list))]):
+        img_id = idx*batch_size + img_num
+        objs = [classes[int(x[-1])] for x in output if int(x[0])==img_id]
+        print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/batch_size))
+        print("{0:20s} {1:s}".format("Objects Detected:", " ".join(objs)))
+        print("----------------------------------------------------------")
+    
+    # make sure CUDA kernel is synchronised with CPU
+    # avoids misleading end = time.time()
+    if CUDA:
+        torch.cuda.synchronize()
