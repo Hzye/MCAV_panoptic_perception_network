fa93d0b7dd235eff9328b7b2213ee252854c957b
Hzye <81663786+Hzye@users.noreply.github.com>
2022-04-26 10:59:32 +1000

    comments

--- utils.py
+++ utils.py
@@ -241,27 +241,36 @@ def load_classes(namesfile):
     names = fp.read().split("\n")[:-1]
     return names
 
-def norm_with_padding(img, inp_dim):
-    '''resize image with unchanged aspect ratio using padding'''
+def norm_with_padding(img, in_dims):
+    """
+    Adds padding to image in order to adhere to size (in_dims, in_dims, 3).
+
+    Does not change aspect ratio.
+    """
     img_w, img_h = img.shape[1], img.shape[0]
-    w, h = inp_dim
+    w, h = in_dims
+
+    # calculate new width and height
     new_w = int(img_w * min(w/img_w, h/img_h))
     new_h = int(img_h * min(w/img_w, h/img_h))
     resized_image = cv2.resize(img, (new_w,new_h), interpolation = cv2.INTER_CUBIC)
     
-    canvas = np.full((inp_dim[1], inp_dim[0], 3), 128)
+    canvas = np.full((in_dims[1], in_dims[0], 3), 128)
 
     canvas[(h-new_h)//2:(h-new_h)//2 + new_h,(w-new_w)//2:(w-new_w)//2 + new_w,  :] = resized_image
     
     return canvas
 
-def prep_image(img, inp_dim):
+def prep_image(img, in_dims):
     """
     Prepare image for inputting to the neural network. 
     
     Returns a Variable 
     """
-    img = (norm_with_padding(img, (inp_dim, inp_dim)))
+    # first pad the image
+    img = (norm_with_padding(img, (in_dims, in_dims)))
+    # then transpose to correct dims order and tensorise 
     img = img[:,:,::-1].transpose((2,0,1)).copy()
     img = torch.from_numpy(img).float().div(255.0).unsqueeze(0)
+    
     return img
