8418ef754fda370fce7a68b57b55d56df52d4ad3
Hzye <81663786+Hzye@users.noreply.github.com>
2022-07-24 14:18:48 +1000

    clean up - packages

--- utils.py
+++ utils.py
@@ -1,9 +1,6 @@
 from __future__ import division
 
-import torch 
-import torch.nn as nn
-import torch.nn.functional as F 
-from torch.autograd import Variable
+import torch
 import numpy as np
 import cv2 
 
